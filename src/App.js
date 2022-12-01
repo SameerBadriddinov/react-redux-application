26ba8d30187ce5420dbb092b44cd001d7595b70e
Samar <samarbadriddinov.tv@gmail.com>
2022-12-01 20:25:52 +0200

    #7. Full-Stack loyiha start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import {Main, Login, Register} from './components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div>
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
+			</Routes>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export {default as Main} from './main'
+export {default as Register} from './register'
+export {default as Login} from './login'
