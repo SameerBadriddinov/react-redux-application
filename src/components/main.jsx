6661a8688c4d9ab4de91419b4691bb29f7a8926c
Samar <samarbadriddinov.tv@gmail.com>
2022-12-03 19:18:45 +0200

    #21. Loader

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,12 @@
 import {useSelector} from 'react-redux'
+import {Loader} from '../ui'
 
 const Main = () => {
-	const {articles} = useSelector(state => state.article)
+	const {articles, isLoading} = useSelector(state => state.article)
 
 	return (
 		<div className='container'>
+			{isLoading && <Loader />}
 			<div className='album py-5 '>
 				<div className='container'>
 					<div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
@@ -26,22 +28,22 @@ const Main = () => {
 									</svg>
 
 									<div className='card-body'>
-										<p className='card-text fw-bold'>{item.title}</p>
+										<p className='card-text fw-bold m-0'>{item.title}</p>
 										<p className='card-text'>{item.description}</p>
-										<div className='d-flex justify-content-between align-items-center'>
-											<div className='btn-group'>
-												<button type='button' className='btn btn-sm btn-outline-success'>
-													View
-												</button>
-												<button type='button' className='btn btn-sm btn-outline-secondary'>
-													Edit
-												</button>
-												<button type='button' className='btn btn-sm btn-outline-danger'>
-													Delete
-												</button>
-											</div>
-											<small className='text-muted fw-bold text-capitalize'>{item.author.username}</small>
+									</div>
+									<div className='card-footer d-flex justify-content-between align-items-center'>
+										<div className='btn-group'>
+											<button type='button' className='btn btn-sm btn-outline-success'>
+												View
+											</button>
+											<button type='button' className='btn btn-sm btn-outline-secondary'>
+												Edit
+											</button>
+											<button type='button' className='btn btn-sm btn-outline-danger'>
+												Delete
+											</button>
 										</div>
+										<small className='text-muted fw-bold text-capitalize'>{item.author.username}</small>
 									</div>
 								</div>
 							</div>
---
+++ src/ui/loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+	return (
+		<div class='spinner-border d-block mx-auto' role='status'>
+			<span class='visually-hidden'>Loading...</span>
+		</div>
+	)
+}
+
+export default Loader
